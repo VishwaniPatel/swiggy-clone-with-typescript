ac870794439f71b7d67833c1de18c9752c3e7006
Vishwani Patel <vishwani.patel@1rivet.local>
2023-07-19 10:58:19 +0530

    feat: added tsconfig.json

---
+++ index.js
@@ -0,0 +1,28 @@
+require("./index.css");
+var $8C61O$reactjsxruntime = require("react/jsx-runtime");
+var $8C61O$react = require("react");
+var $8C61O$reactdomclient = require("react-dom/client");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+
+
+const $01e6b6346b49dc39$var$App = ()=>{
+    return /*#__PURE__*/ (0, $8C61O$reactjsxruntime.jsx)("div", {
+        children: "App"
+    });
+};
+var $01e6b6346b49dc39$export$2e2bcd8739ae039 = $01e6b6346b49dc39$var$App;
+
+
+
+(0, ($parcel$interopDefault($8C61O$reactdomclient))).createRoot(document.getElementById("app")).render(/*#__PURE__*/ (0, $8C61O$reactjsxruntime.jsx)((0, ($parcel$interopDefault($8C61O$react))).StrictMode, {
+    children: /*#__PURE__*/ (0, $8C61O$reactjsxruntime.jsx)((0, $01e6b6346b49dc39$export$2e2bcd8739ae039), {})
+}));
+
+
+//# sourceMappingURL=index.js.map
