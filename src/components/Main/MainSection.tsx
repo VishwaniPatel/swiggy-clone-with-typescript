11839da4715c8e0287960fff231af7dbbb577202
Vishwani Patel <vishwani.patel@1rivet.local>
2023-07-20 16:20:54 +0530

    feat: home page design

--- src/components/Main/MainSection.tsx
+++ src/components/Main/MainSection.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FETCH_RESTAURANTS } from "../../service/restaurantDataService";
+import RestaurantList from "../RestaurantList";
+import { RestaurantModel } from "../model/RestaurantModel";
 
-const MainSection = () => {
-  return <div>MainSection</div>;
+const MainSection: React.FC = () => {
+  const [allRestaurants, setAllRestaurants] = useState<RestaurantModel[]>([]);
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+  async function getRestaurants() {
+    //API CALL to fetch list of restaurants
+    const data = await fetch(FETCH_RESTAURANTS);
+    const json = await data.json();
+    console.log("Vite", json);
+    if (json) {
+      setAllRestaurants(json.data?.cards[2]?.data?.data?.cards);
+      console.log(allRestaurants);
+    }
+  }
+  return (
+    <div>
+      <RestaurantList restaurants={{ data: allRestaurants }} />
+    </div>
+  );
 };
 
 export default MainSection;
